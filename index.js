7e59061648bec0b4659b77d90b9dd46bbda942df
Dhruv <dhruvchandak5@gmai;.com>
2023-05-17 22:09:03 +0530

    Added Some ne Features

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-
-let check=0;
 function Checker() {
     let text = document.getElementById("email").value;
-    console.log(text);
     let count = 0;
     for (let i = 0; i < text.length - 3; i++) {
         if (text[i] == '@') {
@@ -16,24 +13,23 @@ function Checker() {
 
     }
     if (count == 2) {
-        console.log("Email Is Correct");
-        console.log(count);
         document.getElementById("email").style.borderWidth = "3px";
         document.getElementById("email").style.borderColor = "lightgreen";
-        check++;
+        return 1;
 
     }
     else {
-        console.log("Email Is Inorrect");
-        console.log(count);
         document.getElementById("email").style.borderColor = "red";
     }
 }
 function Username() {
+    let text = document.getElementById("username").value;
+    localStorage.setItem("Username", text);
     document.getElementById("username").style.borderWidth = "3px";
     document.getElementById("username").style.borderColor = "lightgreen";
-    check++;
+    return 1;
 }
+
 function pass() {
     let p = document.getElementById("password").value;
     valid();
@@ -53,13 +49,11 @@ function pass() {
         }
     }
     if (a > 0 && b > 0 && c > 0) {
-        console.log("Password Is Correct");
         document.getElementById("password").style.borderWidth = "3px";
         document.getElementById("password").style.borderColor = "lightgreen";
-        check++;
+        return 1;
     }
     else {
-        console.log("Password Is Inorrect");
         document.getElementById("password").style.borderColor = "red";
     }
 }
@@ -68,22 +62,81 @@ function valid() {
     let u = document.getElementById("password").value;
     let g = document.getElementById("Valid").value;
     if (u == g) {
-        console.log("Valid");
         document.getElementById("Valid").style.borderWidth = "3px";
         document.getElementById("Valid").style.borderColor = "lightgreen";
-        check++;
     }
     else {
-        console.log("Invalid");
         document.getElementById("Valid").style.borderColor = "red";
     }
+    return 1;
 }
-function Submit(){
-console.log(check);
-if (check>=4) {
-    console.log("Valid Registration");
-    
+function Generate() {
+    //For number
+    let str = "";
+    for (let i = 1; i < 5; i++) {
+        let x = (Math.floor((Math.random() * 10) + 1)) % 5;
+        switch (i) {
+            case 1: str = str + Math.floor((Math.random() * 10) + 1);
+                break;
+            case 2://Small
+                let c = (Math.floor(Math.random() * 100) % 26 + 97);
+                str = str + String.fromCharCode(c);
+                break;
+            case 3://Capital
+                let d = (Math.floor(Math.random() * 100) % 26 + 65);
+                str = str + String.fromCharCode(d);
+                break;
+            case 4://Symbol
+                let q = (Math.floor((Math.random() * 10) + 1)) % 6;
+                if (q == 1) {
+                    str = str + "!";
+                }
+                if (q == 2) {
+                    str = str + "@";
+                }
+                if (q == 3) {
+                    str = str + "#";
+                }
+                if (q == 4) {
+                    str = str + "$";
+                }
+                if (q == 5) {
+                    str = str + "%";
+                }
+                if (q == 6) {
+                    str = str + "&";
+                }
+                break;
+        }
+    }
+    document.getElementById("generate").innerHTML = str;
+    var text = str;
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+
+    document.getElementById("Clipboard").style.display = "block";
 }
 
 
+function welcome() {
+    let us = localStorage.getItem("Username");
+    document.getElementById("Welcome").innerHTML = `Welcome ${us} `;
 }
+
+function Submit() {
+
+    let output = Username()
+    output += Checker();
+    output += pass();
+    output += valid();
+
+    if (output >= 4) {
+        console.log("Valid Registration");
+        window.open("/Successful.html", '_self');
+    }
+
+}
+
